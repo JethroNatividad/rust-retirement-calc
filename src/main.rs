01631aca5716fd7a8b504835b98dff91b70cf080
JethroNatividad <natividad.jet@gmail.com>
2023-12-19 00:12:50 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Verbs: Determine, prompt, display
+// Nouns: Program, years until retirement, year you can retire, current age, retirement age.
+
+// Inputs: Current age, Retirement age
+// Process: Years until retirement
+// Output: You have {years_till_retirement} left until you can retire. \n It's {current_year}, so you can retire in {retirement_year}
+
 fn main() {
     println!("Hello, world!");
 }
